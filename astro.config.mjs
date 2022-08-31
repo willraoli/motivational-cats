03608d1c79436b1b26435602b9786bf4b6cb20b7
William Oliveira <48337511+willraoli@users.noreply.github.com>
2022-08-31 10:54:48 -0300

    build: add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
+import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
 })
