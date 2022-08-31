62c9c91b199cfa56e2e5375c7ad42e2c37c2eca5
William Oliveira <48337511+willraoli@users.noreply.github.com>
2022-08-31 11:53:31 -0300

    refactor: use URL on api

--- src/api/getCat.ts
+++ src/api/getCat.ts
@@ -1,9 +1,9 @@
 export async function getCat() {
   const res = await fetch(
-    `
+    new URL(`
       ${import.meta.env.PUBLIC_CAT_API_URL}/images/search?api_key=
       ${import.meta.env.CAT_API_KEY}
-    `
+    `)
   )
   const cat = await res.json()
 
--- src/api/getQuote.ts
+++ src/api/getQuote.ts
@@ -1,5 +1,5 @@
 export async function getQuote() {
-  const res = await fetch(import.meta.env.PUBLIC_QUOTE_API_URL)
+  const res = await fetch(new URL(import.meta.env.PUBLIC_QUOTE_API_URL))
   const data = await res.json().catch(err => console.log(err))
   const quote = data[0].q
   const author = data[0].a
